766788a42e33afbd2e0a5f8781827d727b6d7c6b
root <poosan9@gmail.com>
2016-02-23 10:31:30 +0530

    little changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
 
         <form name="login-form" id="login-form" method="post" class="col-md-4 md-padding col-md-offset-4">
 
-            {!! Html::image("img/logo.png","logo",["class"=>"center-block"]) !!}<br/>
+            {!! Html::image("img/logo.png","logo",["class"=>"center-block img-responsive","disabled"=>"disabled"]) !!}<br/>
             <md-card class="md-margin marginL20 marginR20">
                 <md-card-content class="md-padding">
                     <lx-text-field label="Email or Abyasi Id" fixed-label="true" icon="account">
@@ -23,17 +23,14 @@
                         <input type="password" ng-model="input.password" required>
                     </lx-text-field>
                     <div class="md-padding">
-
                         <div class="checkbox marginL20 pull-left" style="margin-top: 5px;">
                             <input type="checkbox" id="checkbox1" ng-model="input.rememberme" class="checkbox__input">
                             <label for="checkbox1" class="checkbox__label">Remember me</label>
                         </div>
-
                         <button type="submit" class="btn btn--m btn--blue btn--raised pull-right" lx-ripple>Login</button>
                     </div>
                 </md-card-content>
             </md-card>
         </form>
-
     </div>
 @stop
